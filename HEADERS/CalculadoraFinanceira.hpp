b0895eabe94a4360893aa5d1c6e2d4f9dc8e7984
netorag <renatoplatz11@outlook.com>
2025-05-05 13:58:40 -0300

    criando o header da calculadora financeira

---
+++ HEADERS/CalculadoraFinanceira.hpp
@@ -0,0 +1,22 @@
+#ifndef CALCULADORA_FINANCEIRA_HPP
+#define CALCULADORA_FINANCEIRA_HPP
+
+#include "CalculadoraCientifica.hpp"
+#include <stdexcept>
+
+class CalculadoraFinanceira : public CalculadoraCientifica {
+public:
+    // Juros compostos (valor futuro)
+    double calcularValorFuturo(double principal, double taxa, int periodos);
+
+    // Valor presente
+    double calcularValorPresente(double valorFuturo, double taxa, int periodos);
+
+    // Pagamentos periódicos (PMT)
+    double calcularPagamentosPeriodico(double double principal, double taxaAnual, int periodos);
+
+    // Taxa interna de retorno (retorno simples)
+    double calcularTaxaRetorno(double investimentoInicial, double retornoTotal, int periodos);
+};
+
+#endif
